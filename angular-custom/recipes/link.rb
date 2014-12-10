f8c2d8707b3635ad75965caab3d17d8ec8b1ba72
Alexandre Salaun <asalaun@siliconsalad.com>
2014-12-10 15:16:32 +0100

    Add debug

--- angular-custom/recipes/link.rb
+++ angular-custom/recipes/link.rb
@@ -1,31 +1,36 @@
 node[:deploy].each do |application, deploy|
-  env          = node[:angular][:env]
-  current_path = deploy[:current_path]
+  Chef::Log.info("Debug : #{deploy[:application_type]}")
+  Chef::Log.info("Debug : #{deploy}")
 
-  Chef::Log.info("Symlink files")
-  Chef::Log.info("Env : #{env}")
+  if defined?(deploy[:application_type]) && deploy[:application_type] == 'static'
+    env          = node[:angular][:env]
+    current_path = deploy[:current_path]
 
-  Chef::Log.info("Path rootScope : #{current_path}/app/js/rootScope/rootScope.#{env}.html")
+    Chef::Log.info("Symlink files")
+    Chef::Log.info("Env : #{env}")
 
-  script 'symlink_rootScope_file' do
-    cwd current_path
-    user 'deploy'
-    code <<-EOH
-      if [ -f app/js/rootScope/rootScope.#{env}.js ]; then
-      ln -sf app/js/rootScope/rootScope.#{env}.js app/js/rootScope/rootScope.js
-      fi
-    EOH
-  end
+    Chef::Log.info("Path rootScope : #{current_path}/app/js/rootScope/rootScope.#{env}.html")
 
-  Chef::Log.info("Path index.html : #{current_path}/app/index.#{env}.html")
+    script 'symlink_rootScope_file' do
+      cwd current_path
+      user 'deploy'
+      code <<-EOH
+        if [ -f app/js/rootScope/rootScope.#{env}.js ]; then
+        ln -sf app/js/rootScope/rootScope.#{env}.js app/js/rootScope/rootScope.js
+        fi
+      EOH
+    end
 
-  script 'symlink_index' do
-    cwd current_path
-    user 'deploy'
-    code <<-EOH
-      if [ -f app/index.#{env}.html ]; then
-      ln -sf app/index.#{env}.html app/index.html
-      fi
-    EOH
+    Chef::Log.info("Path index.html : #{current_path}/app/index.#{env}.html")
+
+    script 'symlink_index' do
+      cwd current_path
+      user 'deploy'
+      code <<-EOH
+        if [ -f app/index.#{env}.html ]; then
+        ln -sf app/index.#{env}.html app/index.html
+        fi
+      EOH
+    end
   end
 end
